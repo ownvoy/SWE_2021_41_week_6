284ed1c52b9a1bb9f5c2cc9c4567dae8c1e84c57
Wonjun Oh <96481582+ownvoy@users.noreply.github.com>
2024-04-11 18:49:42 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    mapp = {}
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in mapp:
+            return [mapp[diff], i]
+        mapp[n] = i
+
+    return []
